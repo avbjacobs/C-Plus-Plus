1ec9463c8a21a206e0bd9ec1c3d141195da6892b
avbjacobs <adityabapat21@gmail.com>
2025-07-19 20:58:27 -0400

    reverted

--- geometry/line_segment_intersection.cpp
+++ geometry/line_segment_intersection.cpp
@@ -87,7 +87,7 @@ struct SegmentIntersection {
 
 /**
  * This is the main function to test whether the algorithm is
- * working correctly.
+ * working well.
  */
 int main() {
     SegmentIntersection segment;
